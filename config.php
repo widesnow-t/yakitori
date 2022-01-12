993483199b2e0bdadaa9668db71c5be7901817a4
widesnow-t <89765385+widesnow-t@users.noreply.github.com>
2022-01-12 14:16:54 +0900

    firstcommit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+define('DSN', 'mysql:host=db;dbname=my_apuri;charset=utf8');
+define('USER', 'my_admin');
+define('PASSWORD','manbo');
+define('MSG_EMAIL_REQUIRED', 'メールアドレスが未入力です');
+define('MSG_NAME_REQUIRED', 'ユーザー名が未入力です');
+define('MSG_PASSWORD_REQUIRED', 'パスワードが未入力です');
+define('MSG_EMAIL_PASSWORD_NOT_MATCH', 'メールアドレスかパスワードが間違ってます');
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/functions.php';
+
+// データベースに接続
+$dbh = connect_db();
+?>
